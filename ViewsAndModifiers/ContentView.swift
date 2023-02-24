b29d053c7a4eb85f3bc4caf93374b551d69258a4
sjgeddam <sgeddam@utexas.edu>
2023-02-23 22:20:17 -0600

    Add notes & demo code from day 23 videos

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -2,20 +2,44 @@
 //  ContentView.swift
 //  ViewsAndModifiers
 //
-//  Created by Ram G on 2/23/23.
+//  Created by Soujanya G on 2/23/23.
 //
 
 import SwiftUI
 
+// TO CONNECT XCODE PROJECT AND GITHUB REPO
+// Go to Xcode project dir on terminal
+// Create github repo on chrome
+// COMMAND TO RUN IN TERMINAL: git remote add origin [https of git here](ex: https://github.com/sjgeddam/ViewsAndModifiers.git)
+
+// Swift UI uses Structs for Views as opposed to Classes for performance reasons.
+// Views become simple inert things that convert data into UI when we use Structs.
+
+// What is behing the main SwiftUI view?
+// For swift UI developers, there is nothing behind our view. (UI hosting controller - connects UIKit with SwiftUI)
+
+// For swift UI developers, please don't reach outside of ContentView. Assume that this is all we have.
+
+// Whenever we apply a modifier to a view, we actually create a copy of that view with the change applied. Your order of the modifier matters - first modifier applies first, second on top of it, and so on. SwiftUI renders your view after every single modifier - not actually how SwiftUI works - but a neat mental short cut while you are learning. As such, we can apply modifiers multiple times.
+
+// Can use ternary conditional operator with conditional modifiers to set views based on true/false
+
+// Need to read documentation on whether a modifier is a environment modifier or regular modifier.
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Griffindor")
+                .font(.largeTitle)
+                .blur(radius: 20)
+            Text("Hufflepuff")
+            Text("Ravenclaw")
+            Text("Slytherin")
+                .bold()
         }
-        .padding()
+        .font(.title) // environment moditfier - applied to every element in vstack. but child's version will take priority
+        .blur(radius: 5) // not an environment modifier, regular modifier - child modifier adds up
+        
     }
 }
 
